59753909e75929efd4631ba9e0d688b5df2355ae
pandabucks <plus.quolity@gmail.com>
2019-07-04 18:23:43 +0900

    calc action implement

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,4 +9,25 @@ class HomeController < ApplicationController
             render plain: "SUCCESS"
         end
     end
+
+    def calc
+        is_match = request.url.match(/\?.*/)
+
+        # ?でパラメータが渡ってこない場合
+        render plain: "ERROR" and return unless is_match
+        calc = request.url.match(/\?.*/)[0][1..-1]
+
+        # +, -, /, *, (, ), 以外の文字列が入っていた場合はERRORを返す
+        render plain: "ERROR" and return unless calc.match(/\d[*()+-\/]/)
+        begin
+            result = eval(calc)
+        rescue
+            render plain: "ERROR" and return
+        end
+        render plain: result
+    end
+
+    def stocker
+        render plain: "hoge"
+    end
 end
